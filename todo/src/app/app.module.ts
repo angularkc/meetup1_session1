0c22bea775c5c9382c392444ba19ceafc697e919
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2017-11-05 17:16:58 -0600

    newly created app

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { TodoService } from './todo/todo.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [TodoService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
