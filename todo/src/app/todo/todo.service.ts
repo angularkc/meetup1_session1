651eda2cd75ebbda41651280404681ed1bec0309
Steven Barnhurst <stevenbarnhurst@gmail.com>
2017-11-12 11:28:33 -0600

    clean up

--- todo/src/app/todo/todo.service.ts
+++ todo/src/app/todo/todo.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
-import 'rxjs/add/operator/map'
-
 
 @Injectable()
 export class TodoService {
@@ -11,9 +9,6 @@ export class TodoService {
   constructor(private http: HttpClient) { }
 
   public getTodos(): Observable<any[]> {
-    return this.http.get<Array<any>>(environment.apiUrl).map((res) => {
-        console.log(res);
-        return res;
-    });
+    return this.http.get<Array<any>>(environment.apiUrl);
   }
 }
